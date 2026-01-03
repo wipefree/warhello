c13edf81c048acc6db4bac917e9bc7317e4fc930
wipefree <wipefree@gmail.com>
2026-01-03 13:43:42 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,5 +6,4 @@ sudo systemctl start docker
 cd /tmp/prj
 mvn package -DskipTests
 docker build -f Dockerfile.builder -t cr.yandex/crpirf1t243rd4chrqfd/hello:latest .
-#docker login -u {{ docker_user }} -p {{ docker_pass }}
-#docker push {{ docker_user }}/{{ img_name }}
+docker push cr.yandex/crpirf1t243rd4chrqfd/hello:latest
