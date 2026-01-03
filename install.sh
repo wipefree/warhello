98baa018161a9f59fed20310016602cfdaa34b75
wipefree <wipefree@gmail.com>
2026-01-03 19:24:13 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+repo="crpirf1t243rd4chrqfd"
+img="hello:latest"
+
 sudo apt-get update
 sudo apt-get -y maven docker.io
 sudo systemctl start docker
 cd /tmp/prj
 sudo mvn package -DskipTests
 sudo cat /tmp/key.json | docker login --username json_key --password-stdin cr.yandex
-sudo docker build -f Dockerfile.builder -t cr.yandex/crpirf1t243rd4chrqfd/hello:latest .
-sudo docker push cr.yandex/crpirf1t243rd4chrqfd/hello:latest
+sudo docker build -f Dockerfile.builder -t cr.yandex/$repo/$img .
+sudo docker push cr.yandex/$repo/$img
